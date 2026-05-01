d6d7f2bc2a0e317551f12e3debea5ade2ccc3a53
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2026-05-01 19:42:24 +0100

    fix: replace wildcard route with app.use for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -364,7 +364,7 @@ app.delete("/api/invoices/:id", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   const clientDist = path.join(__dirname, "../client/dist");
   app.use(express.static(clientDist));
-  app.get("*", (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.join(clientDist, "index.html"));
   });
 }
